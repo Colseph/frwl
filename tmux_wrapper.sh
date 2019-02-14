769bc68c05ae92836d3112fcc9d93abc921b15fd
Cole Richardson <cole.m.richardson.mail@gmail.com>
2019-02-14 10:25:02 -0700

    added recommendations

--- tmux_wrapper.sh
+++ tmux_wrapper.sh
@@ -12,7 +12,7 @@ its also recommended to use a static path for WORKING_DIR also IF your not runni
 then sessions sharing ips will pool files - problems:
 	- clobbering (w/ 500 instances running, your bound to get duplicates)
 	- incorrect ITER numbers(this is something w/ the iteration script in general)(but since its the timestamps were after,
-		i dont think itll cause massive problems
+	i dont think itll cause massive problems)
 ie.
 "/home/$USER/downloads/git/frwl/frwl_tarballs"
 "/home/$USER/downloads/git/frwl/servers.txt"
@@ -20,6 +20,15 @@ ie.
 otherwise youll have to fish the tarballs out of each of the sub directories.
 and the script will fail because it cant find ./servers.txt
 (ping_russia.sh will automatically put tarballs in subdirectories by server name)
+
+its also recommented to create a new servers.txt with the total amount of servers you want to trace
+ie 
+	`mv servers.txt servers_original.txt; shuf -n 400 servers_original.txt` > servers.txt` #now servers.txt only has 400 randomly selected ips in it
+this way youll get max of 20GB raw data, (50M*400ServersAvailable)/1000M vs this example:
+if i had ping_russia.sh set to 10 servers, and i had 500 instances running in tmux, thers a POSSIBILITY(if every server is unique)
+to be tracing 5000 servers and have a max of (10Servers*500Instances*50M)/1000M = 250GB of raw uncompressed data..
+
+but idk you now your system, you can choose what you want to do.
 
 also this is all an experiment, ive never done this before and idk how resource intensive it is, so youll want to change stuff according to your setup
 '
@@ -33,7 +42,7 @@ TMUX_DIR="./tmux_dir" #all the subdirectories for each tmux session will be in h
 PING_RUSSIA="$( cd "$(dirname "$0")" ; pwd -P )/ping_russia.sh" #location of ping_russia.sh - default same dir as tmux_wrapper.sh
 SESSION_NUM=500 #idk you might want to change this
 SESSION_NAME="FRWLx$SESSION_NUM" #name for the tmux session
-NAMING_FORMAT='frwl-%03d' #printf - for directory and tmux window names - crazy people can change the padding to %03d etc for over 999 sessions...
+NAMING_FORMAT='frwl-%03d' #printf - for directory and tmux window names - crazy people can change the padding to %04d etc for over 999 sessions...
 
 #~~~~~~~~~~~~~#
 #~ Functions ~#

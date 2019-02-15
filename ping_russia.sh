3c08fa4ef6ca69af4bb70fb4f499c3223c1816dc
Colseph <cole.m.richardson.mail@gmail.com>
2019-02-15 14:07:58 -0700

    [most of this commit is NOT my work] incorperated some changes from LogioLab master, but i didnt want to 'git cherry-pick'

--- ping_russia.sh
+++ ping_russia.sh
@@ -24,67 +24,77 @@ DEPENDENCIES=(traceroute tar); #not every distro has these pre-installed
 #~~~~~~~~~~~~~#
 
 _log() {
-	#~just a log function
-	#~case logic for nice formating
-	#~logging is kept to a minimum to keep log files small
-	case $1 in
-		date)
-			#~appends date to front
-			printf '%s\n' "`date +%Y-%m-%d_%T` ---  $2" >> $LOG_FILE
-			;;
-		*)
-			#~just logs
-			printf '%s\n' "$@" >> $LOG_FILE
-			;;
-	esac
+    #~just a log function
+    #~case logic for nice formating
+    #~logging is kept to a minimum to keep log files small
+    case $1 in
+        date)
+            #~appends date to front
+            printf '%s\n' "`date +%Y-%m-%d_%T` ---  $2" >> $LOG_FILE
+            ;;
+        *)
+            #~just logs
+            printf '%s\n' "$@" >> $LOG_FILE
+            ;;
+    esac
 }
 
 _checkPath() {
-	#~verifies paths exist/creates if needed
-	[ -e "$1" ] || mkdir -p "$1"
-	_log date "[_checkPath]checking directory $1"
+    #~verifies paths exist/creates if needed
+    [ -e "$1" ] || mkdir -p "$1"
+    _log date "[_checkPath]checking directory $1"
 }
 
 _tarBall() {
-	#~creates tarball of collected data with id/timestamp
-	tar cjf "$TARBALL_DIR/$COMP_ITER.$TIME.$SERVER.tar.xz" "$WORKING_DIR"/* --remove-files
-	_log date "[_tarBall]created tarball '$COMP_ITER.$TIME.$SERVER.tar.xz'"
-	COMP_ITER=$(( COMP_ITER + 1 ))
-	ITER=0
-	_updateDirs
+    #~creates tarball of collected data with id/timestamp
+    tar cjf "$TARBALL_DIR/$COMP_ITER.$TIME.$SERVER.tar.xz" "$WORKING_DIR"/* && rm -rf "$WORKING_DIR"/*
+    _log date "[_tarBall]created tarball '$COMP_ITER.$TIME.$SERVER.tar.xz'"
+    COMP_ITER=$(( COMP_ITER + 1 ))
+    ITER=0
+    _updateDirs
 }
 
 _updateDirs() {
-	#~check if if theres any updates to SERVER_LIST and make required directories
-	while read LINE; do
-		if [[ ! "$LINE" == *"#"* ]]; then
-			_checkPath "$WORKING_DIR/$LINE"
-			_checkPath "$TARBALL_DIR/$LINE"
-		fi
-	done < "./selected_servers.txt"
+    #~check if if theres any updates to SERVER_LIST and make required directories
+    while read LINE; do
+        if [[ ! "$LINE" == *"#"* ]]; then
+            _checkPath "$WORKING_DIR/$LINE"
+            _checkPath "$TARBALL_DIR/$LINE"
+        fi
+    done < "./selected_servers.txt"
 }
 
 _verifyServerList() {
-	#~gets user defined amount of random lines from $SERVER_LIST
-	#~if server_new.txt doesnt already exist
-	#~might grab comments, so selected servers might be less than wanted
-	if [ -e "./selected_servers.txt" ]; then
-		_log date "[_verifyServerList]selected_servers.txt already exists"
-	else
-		_log date "[_verifyServerList]selected_servers.txt not found, creating w/ ($SERVER_NUM) ips"
-	       	shuf -n $SERVER_NUM "$SERVER_LIST" > "./selected_servers.txt"
-	fi
-	_log date "[_verifyServerList]server_new.txt verified with ips: "
-	_log "$(cat "./selected_servers.txt")"
+    #~gets user defined amount of random lines from $SERVER_LIST
+    #~if server_new.txt doesnt already exist
+    #~might grab comments, so selected servers might be less than wanted
+    if [ -e "./selected_servers.txt" ]; then
+        _log date "[_verifyServerList]selected_servers.txt already exists"
+    else
+        _log date "[_verifyServerList]selected_servers.txt not found, creating w/ ($SERVER_NUM) ips"
+            shuf -n $SERVER_NUM "$SERVER_LIST" > "./selected_servers.txt"
+    fi
+    _log date "[_verifyServerList]selcted_servers.txt verified with ips: "
+    _log "$(cat "./selected_servers.txt")"
 }
 
 _checkDeps() {
-	#~check dependencies
-	for p in "${DEPENDENCIES[@]}"; do
-		if ! [ -x "$(command -v $p)" ]; then
-		echo "$p is not installed"; exit 1;
-	    fi
-	done
+    #~check dependencies
+    for p in "${DEPENDENCIES[@]}"; do
+        if ! [ -x "$(which $p)" ]; then
+            echo "$p is not installed or in the scripts PATH"; exit 1;
+        fi
+    done
+
+    if [ ! -f ./servers.txt ] ; then
+      echo "Please provide a servers.txt file with a server per line."
+      exit
+    fi
+}
+
+_randomDir() {
+    #~gets a random 3-level directory
+    echo $(dd if=/dev/urandom bs=512 count=1 2>&1 | md5sum | tail -1 | awk '{print $1}' | cut -b1,2 --output-delimiter=/)
 }
 
 #~~~~~~~~~~~~~~~~#
@@ -93,19 +103,22 @@ _checkDeps() {
 _log date "[main]script start"
 _checkDeps
 _verifyServerList
+
+TIMEZONE=$(date +”%Z”)
+_log date "[main]timezone: $TIMEZONE"
 while true
 do
-	_updateDirs
-	while read LINE; do
-		if [[ ! "$LINE" == *"#"* ]] && [[ -n "$LINE" ]]; then
-			#~filters comments(#) and blanks lines
-			SERVER=$LINE #only for better readability
-			TIME=$(date +%s)
-			SIZE=$(du -B 50M "$WORKING_DIR/$SERVER/" | cut -d "	" -f 1)
-			traceroute -I $SERVER > "$WORKING_DIR/$SERVER/$ITER.$TIME.old"
-			ITER=$(( ITER + 1 ))
-			[ $SIZE -gt 1 ] && _tarBall
-			traceroute -I $SERVER > "$WORKING_DIR/$SERVER/$ITER.$TIME.new"
-		fi
-	done < "./selected_servers.txt"
+    _updateDirs
+    while read LINE; do
+        if [[ ! "$LINE" == *"#"* ]] && [[ -n "$LINE" ]]; then
+            #~filters comments(#) and blanks lines
+            SERVER=$LINE #only for better readability
+            TIME=$(date +%s)
+            SIZE=$(du -s -B 50M "${WORKING_DIR}" | awk '{printf $1}')
+            traceroute -I $SERVER > "$WORKING_DIR/$SERVER/$ITER.$TIME.old"
+            ITER=$(( ITER + 1 ))
+            [ $SIZE -gt 1 ] && _tarBall
+            traceroute -I $SERVER > "$WORKING_DIR/$SERVER/$ITER.$TIME.new"
+        fi
+    done < "./selected_servers.txt"
 done

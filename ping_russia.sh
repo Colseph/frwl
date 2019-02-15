3c6c36fcfe48dd4a00975cb1ebee574674a33a6b
Colseph <cole.m.richardson.mail@gmail.com>
2019-02-15 16:45:04 -0700

    implimented fix for issue #2 and drastically increased logging for easier debugging

--- ping_russia.sh
+++ ping_russia.sh
@@ -10,14 +10,18 @@
 #~ Variables ~#
 #~~~~~~~~~~~~~#
 
-LOG_FILE="/dev/null" #logging is disabled by default as we want to fill out HDDs w/ DATA not un-needed logs
-ITER=0
-COMP_ITER=0
+LOG_FILE="frwl.log" #log is for debugging, and thus very verbose, recommend to set to /dev/null to disable
 SERVER_LIST="./servers.txt" #insert IP list here
 SERVER_NUM=10 #seems like a decent enough default value?
 WORKING_DIR="./working_dir" #directory for uncompressed raw data
 TARBALL_DIR="./from_russia_with_love_comp" #directory for compressed tarballs
+
+#~~~stuff below this shouldnt need editing~~~(unless you know what your doing)
+TIMEZONE=$(date +”%Z”)
 DEPENDENCIES=(traceroute tar); #not every distro has these pre-installed
+ITER_SAVE_FILE="./iter_save" #file for keeping track of ITER values
+COMP_ITER_SAVE_FILE="./comp_iter_saves" #file for keeping track of COMP_ITER values
+SAVE_FILE_DELIM='---' #delimiter used in save files. its escaped so you can use almost anything. parenthesis will break it, and maybe other characters i havent tested
 
 #~~~~~~~~~~~~~#
 #~ Functions ~#
@@ -26,7 +30,6 @@ DEPENDENCIES=(traceroute tar); #not every distro has these pre-installed
 _log() {
     #~just a log function
     #~case logic for nice formating
-    #~logging is kept to a minimum to keep log files small
     case $1 in
         date)
             #~appends date to front
@@ -49,8 +52,9 @@ _tarBall() {
     #~creates tarball of collected data with id/timestamp
     tar cjf "$TARBALL_DIR/$COMP_ITER.$TIME.$SERVER.tar.xz" "$WORKING_DIR"/* && rm -rf "$WORKING_DIR"/*
     _log date "[_tarBall]created tarball '$COMP_ITER.$TIME.$SERVER.tar.xz'"
-    COMP_ITER=$(( COMP_ITER + 1 ))
-    ITER=0
+    COMP_ITER=$(_increment "$COMP_ITER_SAVE_FILE")
+    _log date "[_tarBall]COMP_ITER: $COMP_ITER"
+    ITER=$(_increment 0 "$ITER_SAVE_FILE")
     _updateDirs
 }
 
@@ -71,7 +75,7 @@ _verifyServerList() {
     if [ -e "./selected_servers.txt" ]; then
         _log date "[_verifyServerList]selected_servers.txt already exists"
     else
-        _log date "[_verifyServerList]selected_servers.txt not found, creating w/ ($SERVER_NUM) ips"
+        _log date "[_verifyServerList]selected_servers.txt not found, creating w/ up to ($SERVER_NUM) ips"
             shuf -n $SERVER_NUM "$SERVER_LIST" > "./selected_servers.txt"
     fi
     _log date "[_verifyServerList]selcted_servers.txt verified with ips: "
@@ -82,19 +86,57 @@ _checkDeps() {
     #~check dependencies
     for p in "${DEPENDENCIES[@]}"; do
         if ! [ -x "$(which $p)" ]; then
-            echo "$p is not installed or in the scripts PATH"; exit 1;
+            _log date "[_checkDeps]FATAL: $p is not installed or in the scripts PATH. exiting.."
+            printf '%s\n' "FATAL: $p is not installed or in the scripts PATH. exiting.."; exit 1;
         fi
     done
 
-    if [ ! -f ./servers.txt ] ; then
-      echo "Please provide a servers.txt file with a server per line."
+    if [ ! -f "$SERVER_LIST" ] ; then
+        _log date "[_checkDeps]FATAL: missing $SERVER_LIST exiting.."
+      printf '$s\n' "FATAL: missing server list! please check the \$SERVER_LIST variable and verify '$SERVER_LIST' exists. exiting.."
       exit
     fi
 }
 
+_increment() {
+    #~keeps ITER and COMP_ITER straight using a save file of sorts
+    #~takes option and filename as arguments
+    case $1 in
+        0)
+            #~triggers on tar - sets iter back to zero
+            [[ "$(cat "$2")" == *"${SERVER}${SAVE_FILE_DELIM}"* ]] || printf '%s\n' "${SERVER}${SAVE_FILE_DELIM}0" >> "$2"
+            sed -i "s/${SERVER}$(_escapeString "$SAVE_FILE_DELIM")\([0-9]\+\)/${SERVER}$(_escapeString "$SAVE_FILE_DELIM")0/g" "$2"
+            printf '%s' '0' #returns 0 as value
+            _log date "[_increment]set value for $SERVER in $2 to 0"
+            ;;
+        *)
+            #~adds to save file it doesnt exist
+            [[ "$(cat "$1")" == *"${SERVER}${SAVE_FILE_DELIM}"* ]] || printf '%s\n' "${SERVER}${SAVE_FILE_DELIM}0" >> "$1"
+            #~grabs current iter state from list
+            RETURN_VAL=$(cat "$1" | grep "${SERVER}$(_escapeString "$SAVE_FILE_DELIM")")
+            _log date "[_increment]RETURN_VAL: '$RETURN_VAL' (pre parse)"
+            RETURN_VAL=${RETURN_VAL#*"$SAVE_FILE_DELIM"}
+            _log date "[_increment]RETURN_VAL: '$RETURN_VAL' (post parse)"
+            #~increments value in the save file for next read
+            sed -i "s/${SERVER}$(_escapeString "$SAVE_FILE_DELIM")\([0-9]\+\)/${SERVER}$(_escapeString "$SAVE_FILE_DELIM")$(($RETURN_VAL + 1))/g" "$1"
+            printf '%s' "$RETURN_VAL" #retuns value read from $1
+            _log date "[_increment]read value for $SERVER in $1 as $RETURN_VAL, and set new as $(($RETURN_VAL + 1)) "
+            ;;
+    esac
+}
+
 _randomDir() {
     #~gets a random 3-level directory
-    echo $(dd if=/dev/urandom bs=512 count=1 2>&1 | md5sum | tail -1 | awk '{print $1}' | cut -b1,2 --output-delimiter=/)
+    RETURN_VAL=$(dd if=/dev/urandom bs=512 count=1 2>&1 | md5sum | tail -1 | awk '{printf $1}' | cut -b1,2 --output-delimiter=/)
+    printf '%s' "$RETURN_VAL"
+    _log date "[_randomDir]returned value: $RETURN_VAL"
+}
+
+_escapeString() {
+    #~escapes supplied string
+    #~this allows users to use what would normally be considered regex expressions as part of the SAVE_FILE_DELIMITER
+    printf '%q' "$1"
+    _log date "[_escapeString]escaping '$1' --> '$(printf '%q' "$1")'"
 }
 
 #~~~~~~~~~~~~~~~~#
@@ -103,9 +145,19 @@ _randomDir() {
 _log date "[main]script start"
 _checkDeps
 _verifyServerList
+#~log variables
+_log "[main]SCRIPT_VARIABLES:"
+_log "LOG_FILE: $LOG_FILE"
+_log "SERVER_LIST: $SERVER_LIST"
+_log "SERVER_NUM: $SERVER_NUM"
+_log "WORKING_DIR: $WORKING_DIR"
+_log "TARBALL_DIR: $TARBALL_DIR"
+_log "TIMEZONE: $TIMEZONE"
+_log "DEPENDENCIES:[${DEPENDENCIES[@]}]"
+_log "ITER_SAVE_FILE: $ITER_SAVE_FILE"
+_log "COMP_ITER_SAVE_FILE: $COMP_ITER_SAVE_FILE"
+_log "SAVE_FILE_DELIM: '$SAVE_FILE_DELIM'"
 
-TIMEZONE=$(date +”%Z”)
-_log date "[main]timezone: $TIMEZONE"
 while true
 do
     _updateDirs
@@ -115,10 +167,19 @@ do
             SERVER=$LINE #only for better readability
             TIME=$(date +%s)
             SIZE=$(du -s -B 50M "${WORKING_DIR}" | awk '{printf $1}')
+            _log "[main]LOOP_VARIABLES:"
+            _log "SERVER: $SERVER"
+            _log "TIME: $TIME"
+            _log "SIZE: $SIZE"
             traceroute -I $SERVER > "$WORKING_DIR/$SERVER/$ITER.$TIME.old"
-            ITER=$(( ITER + 1 ))
+            ECODE=$?
+            _log "[main]traceroute -I $SERVER completed w/ result: $ECODE"
             [ $SIZE -gt 1 ] && _tarBall
             traceroute -I $SERVER > "$WORKING_DIR/$SERVER/$ITER.$TIME.new"
+            ECODE=$?
+            _log "[main]traceroute -I $SERVER completed w/ result: $ECODE"
+            ITER=$(_increment "$ITER_SAVE_FILE")
+            _log "ITER: $ITER"
         fi
     done < "./selected_servers.txt"
 done
